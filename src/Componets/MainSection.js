f12a2387e5cdac96732978338a2658248271e0dc
Mohamed Ibrahim <54213690+MoShouman@users.noreply.github.com>
2021-12-07 17:49:02 +0200

    Update MainSection.js

--- src/Componets/MainSection.js
+++ src/Componets/MainSection.js
@@ -9,7 +9,7 @@ function MainSection() {
                             <h5>Hello!</h5>
                             <h1 style={{fontSize: '3.5rem', fontWeight:'700'}}>I'm Mohamed Ibrahim</h1>
                             <p style={{fontSize: '2rem', color: '#d3d2d0'}}>Frontend Developer | Software Engineer</p>
-                            <p className="mb-5">Ambition is the path to success. Persistance is the vehicle you arrive in. -Bill Dradley</p>
+                            <p className="mb-5">Ambition is the path to success. Persistance is the vehicle you arrive in.<br/> -Bill Dradley</p>
                             <a className="HireMeBtn" href="#AboutUs">HireMe</a>
                         </Col>
                         <Col sm={12} md={6}>
